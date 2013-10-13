4886e72ab17590215f2dcd71cc474d256cefa291
Jeff Sisson <jeff@bigboy.us>
2013-10-13 03:28:14 +0000

    initial nonsense

--- coder-base/config.js
+++ coder-base/config.js
@@ -3,8 +3,8 @@ exports.listenIP = null; //Defaults to *
 exports.listenPort = '8081'; //the SSL port things run on
 exports.httpListenPort = '8080'; //this will all be redirected to SSL
 exports.cacheApps = true;
-exports.httpVisiblePort = '80'; //forwarded http port the user sees
-exports.httpsVisiblePort = '443'; //forwarded https port the user sees
+exports.httpVisiblePort = '8080'; //forwarded http port the user sees
+exports.httpsVisiblePort = '8081'; //forwarded https port the user sees
 
 
 //SSL Info
--- coder-base/server.js
+++ coder-base/server.js
@@ -65,7 +65,7 @@ var apphandler = function( req, res, appdir ) {
     util.log( "GET: " + apppath + " " + appname );
 
     //Redirect to sign-in for unauthenticated users
-    publicAllowed = ["auth"]; //apps that are exempt from any login (should only be auth)
+    publicAllowed = ["auth","wifi"]; //apps that are exempt from any login (should only be auth)
     auth = require(appdir + "auth" + "/app");
     user = auth.isAuthenticated(req, res);
     if ( !user && publicAllowed.indexOf( appname ) < 0) {
@@ -250,7 +250,7 @@ sslapp.use( express.session({
 sslapp.use( '/static', express.static( __dirname + '/static' ) );
 sslapp.get( '/', function( req, res ) {
     util.log( 'GET: /' );
-    res.redirect( '/app/auth' );
+    res.redirect( '/app/wifi' );
 });
 sslapp.all( /^\/app\/(\w+)\/(.*)$/, function( req, res ) { apphandler( req, res,  __dirname + '/apps/'); } );
 sslapp.all( /^\/app\/(\w+)\/$/, function( req, res ) { apphandler( req, res,  __dirname + '/apps/'); } );
